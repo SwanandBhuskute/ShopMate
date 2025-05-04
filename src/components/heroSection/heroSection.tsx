2c3549fa4520c6302a0fc38345ca388ba0f6d7eb
SwanandBhuskute <swanandbhuskute2003@gmail.com>
2025-05-05 01:02:01 +0530

    img fix

--- src/components/heroSection/heroSection.tsx
+++ src/components/heroSection/heroSection.tsx
@@ -44,7 +44,7 @@ const HeroSection = (props: { products: Product[] }) => {
         {/* Right Column - Image */}
         <div className="w-full lg:w-1/2 flex justify-center">
           <img
-            src="public/hero-img.avif"
+            src="/hero-img.avif"
             alt="hero image"
             className="w-full max-w-[500px] sm:max-w-[600px] h-auto rounded-xl shadow-2xl"
           />
@@ -106,17 +106,6 @@ const HeroSection = (props: { products: Product[] }) => {
 };
 
 export default HeroSection;
-
-
-
-
-
-
-
-
-
-
-
 
 // import { Product } from "../../models/Products.model";
 // import { Link } from "react-router-dom";
@@ -223,13 +212,6 @@ export default HeroSection;
 // };
 
 // export default HeroSection;
-
-
-
-
-
-
-
 
 // import { Product } from "../../models/Products.model";
 // import { Link } from "react-router-dom";
